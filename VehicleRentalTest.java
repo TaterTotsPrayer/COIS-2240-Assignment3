58fde2c9c471f8872e2b78329594ffdea30eb602
Anthdy <dinhanthony05@gmail.com>
2025-11-30 22:25:48 -0500

    Added testRentAndReturnVehicle method

--- VehicleRentalTest.java
+++ VehicleRentalTest.java
@@ -60,7 +60,6 @@ class VehicleRentalTest {
         });
 	}
 	
-	
 	@Test
 	void testRentAndReturnVehicle() {
 		
@@ -79,15 +78,5 @@ class VehicleRentalTest {
 		
 		boolean returnAgain = rentalSystem.returnVehicle(vehicle, customer, LocalDate.now(), 0.0);
 		assertFalse(returnAgain, "Returning same vehicle fails");
-	}
-	
-	@Test
-	void testSingletonRentalSystem() throws Exception{
-		Constructor<RentalSystem> constructor = RentalSystem.class.getDeclaredConstructor();
-		int modifiers = constructor.getModifiers();
-		assertTrue(Modifier.isPrivate(modifiers), "Constructor is private");
-		
-		RentalSystem instance = RentalSystem.getInstance();
-		assertNotNull(instance, "Singleton instance not null");
 	}
 }
