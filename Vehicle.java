228484c33b7fc90ff000ac5469631f10041fefed
Anthdy <dinhanthony05@gmail.com>
2025-11-30 22:13:33 -0500

    Updated Vehicle.java to validate license plate input

--- Vehicle.java
+++ Vehicle.java
@@ -1,27 +1,5 @@
+
 public abstract class Vehicle {
-    private String licensePlate;
-    private String make;
-    private String model;
-    private int year;
-    private VehicleStatus status;
-
-    public enum VehicleStatus { Available, Held, Rented, UnderMaintenance, OutOfService }
-
-    public Vehicle(String make, String model, int year) {
-    	if (make == null || make.isEmpty())
-    		this.make = null;
-    	else
-    		this.make = make.substring(0, 1).toUpperCase() + make.substring(1).toLowerCase();
-    	
-    	if (model == null || model.isEmpty())
-    		this.model = null;
-    	else
-    		this.model = model.substring(0, 1).toUpperCase() + model.substring(1).toLowerCase();
-    	
-        this.year = year;
-        this.status = VehicleStatus.Available;
-        this.licensePlate = null;
-    }public abstract class Vehicle {
     private String licensePlate;
     private String make;
     private String model;
@@ -38,7 +16,7 @@ public abstract class Vehicle {
     public enum VehicleStatus { Available, Held, Rented, UnderMaintenance, OutOfService }
 
     public Vehicle(String licensePlate, String make, String model, int year) {
-    	this.licensePlate = licensePlate;
+    	setLicensePlate(licensePlate);
     	this.make = capitalize(make);
     	this.model = capitalize(model);
         this.year = year;
@@ -50,6 +28,9 @@ public abstract class Vehicle {
     //}
 
     public void setLicensePlate(String plate) {
+    	if(!validPlate(plate)) {
+    		throw new IllegalArgumentException("Invalid license plate");
+    	}
         this.licensePlate = plate == null ? null : plate.toUpperCase();
     }
 
@@ -71,32 +52,11 @@ public abstract class Vehicle {
         return "| " + licensePlate + " | " + make + " | " + model + " | " + year + " | " + status + " |";
     }
 
-}
-
-    public Vehicle() {
-        this(null, null, 0);
+    private boolean validPlate(String plate) {
+    	if(plate == null || plate.isEmpty()) {
+    		return false;
+    	}
+    	
+    	return plate.matches("^[A-Z]{3}[0-9]{3}$");
     }
-
-    public void setLicensePlate(String plate) {
-        this.licensePlate = plate == null ? null : plate.toUpperCase();
-    }
-
-    public void setStatus(VehicleStatus status) {
-    	this.status = status;
-    }
-
-    public String getLicensePlate() { return licensePlate; }
-
-    public String getMake() { return make; }
-
-    public String getModel() { return model;}
-
-    public int getYear() { return year; }
-
-    public VehicleStatus getStatus() { return status; }
-
-    public String getInfo() {
-        return "| " + licensePlate + " | " + make + " | " + model + " | " + year + " | " + status + " |";
-    }
-
 }
